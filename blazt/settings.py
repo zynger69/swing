a681f90b05fbc30fb859d69872de5fc0e6ae9e18
zynger69 <147348089+zynger69@users.noreply.github.com>
2023-10-09 14:42:02 +0530

    Update settings.py

--- blazt/settings.py
+++ blazt/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'blazt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'swing-postgres',
         'USER':os.environ["POSTGRES_USER"] ,
         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
